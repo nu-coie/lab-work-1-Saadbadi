42acacdbd12e85f0ab8b5968627e1d515951a9a7
Saadbadi <saad.badie93@gmail.com>
2023-04-10 08:15:36 +0000

    ddd, Apr 4/10/2023, YYYY, 8:15 A +0

--- calculator.c
+++ calculator.c
@@ -6,16 +6,14 @@ int main(void)
 {
 // Accept Input From user (char), where (a) stands for "Addition", (m) for "Multiplications," and (d) for "Division,"
 
-char op = get_char("op:   ");
+char input = get_char("op:   ");
+ int x = get_int("X:   ");
+int y = get_int("Y:    ");
 
-if (op == 'a')
+if (input == 'a' || input == 'A')
 {
-    int x = get_int("X:   ");
-    int y = get_int("Y:    ");
-
     int z = x + y;
-
-    
+    printf("%i\n",z);
 }
 
 
